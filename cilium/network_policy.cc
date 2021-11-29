accuknox/cilium-proxy-old-private

Allow wildcard for CNP
This commit allow a wildcard to be used inside a Cilium Network Policy.
The wildcard '*' (used by the user in the policy) is going to be
translate to '.+' for internal use of envoy regex.

Signed-off-by: Raphael Campos <raphael@accuknox.com>

cilium/network_policy.cc
@@ -478,10 +478,22 @@ class PolicyInstanceImpl : public PolicyInstance {
       return dst_port_;
     }
 
+    std::string replaceWildcardCNP(std::string str, const std::string& search,
+                                  const std::string& replace) {
+      size_t pos = 0;
+      while ((pos = str.find(search, pos)) != std::string::npos) {
+        str.replace(pos, search.length(), replace);
+        pos += replace.length();
+      }
+
+      return str;
+    }
+
     void processSpiffe(const ::cilium::Spiffe& spiffe) {
       is_spiffe_ = true;
       for (int i = 0; i < spiffe.peer_ids_size(); i++) {
-        spiffe_peer_ids_.push_back(spiffe.peer_ids(i));
+        std::string spiffeIDRegex = replaceWildcardCNP(spiffe.peer_ids(i), "*", ".+");
+        spiffe_peer_ids_.push_back(spiffeIDRegex);
       }
     }
 

accuknox/cilium-proxy

cilium/network_policy.cc
@@ -478,10 +478,22 @@ class PolicyInstanceImpl : public PolicyInstance {
       return dst_port_;
     }
 
+    std::string findReplace(std::string str, const std::string& search,
+                                  const std::string& replace) {
+      size_t pos = 0;
+      while ((pos = str.find(search, pos)) != std::string::npos) {
+        str.replace(pos, search.length(), replace);
+        pos += replace.length();
+      }
+
+      return str;
+    }
+
     void processSpiffe(const ::cilium::Spiffe& spiffe) {
       is_spiffe_ = true;
       for (int i = 0; i < spiffe.peer_ids_size(); i++) {
-        spiffe_peer_ids_.push_back(spiffe.peer_ids(i));
+        std::string spiffeIDRegex = findReplace(spiffe.peer_ids(i), "*", ".+");
+        spiffe_peer_ids_.push_back(spiffeIDRegex);
       }
     }
 